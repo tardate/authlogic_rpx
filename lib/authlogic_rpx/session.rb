5227619c45b067945ba8f7e9f357f880eb06d021
tardate <gallagher.paul@gmail.com>
2009-10-09 20:42:40 +0800

    clarified documentation of the issue of database constraints in auto-mapped fields

--- lib/authlogic_rpx/session.rb
+++ lib/authlogic_rpx/session.rb
@@ -166,17 +166,23 @@ module AuthlogicRpx
 			end
 
 			# map_rpx_data maps additional fields from the RPX response into the user object during auto-registration.
-			# Override this in your session controller to change the field mapping
+			# Override this in your session model to change the field mapping
 			# See https://rpxnow.com/docs#profile_data for the definition of available attributes
 			#
-			# WARNING: if you are using auto-registration, any fields you map should NOT have unique constraints enforced at the database level.
+			# WARNING: if you are using auto-registration, any fields you map should NOT have constraints enforced at the database level.
 			# authlogic_rpx will optimistically attempt to save the user record during registration, and 
-			# violating a unique constraint will cause the authentication/registration to fail.
+			# violating a database constraint will cause the authentication/registration to fail.
 			#
 			# You can/should enforce any required validations at the model level e.g.
 			#   validates_uniqueness_of   :username, :case_sensitive => false
 			# This will allow the auto-registration to proceed, and the user can be given a chance to rectify the validation errors
 			# on your user profile page.
+			#
+			# If it is not acceptable in your application to have user records created with potential validation errors in auto-populated fields, you
+			# will need to override map_rpx_data and implement whatever special handling makes sense in your case. For example:
+			#   - directly check for uniqueness and other validation requirements
+			#   - automatically "uniquify" fields like username
+			#   - save conflicting profile information to "pending user review" columns or a seperate table
 			#
 			def map_rpx_data
 				self.attempted_record.send("#{klass.login_field}=", @rpx_data['profile']['preferredUsername'] ) if attempted_record.send(klass.login_field).blank?
