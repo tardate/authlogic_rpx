06db2347e3d27d566376b6a8e729ab1d50043dcf
tardate <gallagher.paul@gmail.com>
2010-01-03 15:51:44 +0800

    add test of disabled auto-registration (non-mapped)

--- test/integration/no_mapping/basic_test.rb
+++ test/integration/no_mapping/basic_test.rb
@@ -1,13 +1,5 @@
 require File.dirname(__FILE__) + '/../../test_helper.rb'
 
-#class User
-#	def map_added_rpx_data( rpx_data )
-		# map email field
-#		self.photo_url = rpx_data['profile']['photo'] if photo_url.blank?
-#	end
-
-#end
-
 class BasicTest < ActiveSupport::TestCase
 
   must "authenticate valid existing user" do
@@ -32,7 +24,6 @@ class BasicTest < ActiveSupport::TestCase
     UserSession.auto_register true
     User.account_merge_enabled false
     User.account_mapping_mode :none
-    #User.validate_email_field false
     
     # get response template. set the controller token (used by RPX mock to match mock response)
     test_user = rpxresponses(:unregistered_rpx_auth_user_one)
@@ -44,5 +35,19 @@ class BasicTest < ActiveSupport::TestCase
     assert_true session.registration_complete?, "registration should be complete"
   end
  
-  
+
+  must "auto-register disabled for an unregistered user" do
+    # enforce Authlogic settings required for test
+    UserSession.auto_register false
+    User.account_merge_enabled false
+    User.account_mapping_mode :none
+    
+    # get response template. set the controller token (used by RPX mock to match mock response)
+    test_user = rpxresponses(:unregistered_rpx_auth_user_one)
+    controller.params[:token] = test_user.username
+    
+    session = UserSession.new
+    assert_false session.save, "should not be a valid session"
+  end
+
 end
