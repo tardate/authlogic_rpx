aa1b093aeac6aa9f706a08acfb237205a77277f0
tardate <gallagher.paul@gmail.com>
2010-01-05 00:10:49 +0800

    support all rpx_now options in popup/embed code; github issue #6

--- lib/authlogic_rpx/helper.rb
+++ lib/authlogic_rpx/helper.rb
@@ -22,20 +22,28 @@ module AuthlogicRpx
 		#   * <tt>add_rpx:</tt> if true, requests RPX callback to add to current session. Else runs normal authentication process (default)
 		#   * <tt>unobtrusive:</tt> true/false; sets javascript style for link. Default: true
 		#
-		# NB: i18n considerations? supports a :language parameter (not tested)
+		# The options hash may include other options as supported by rpx_now (see http://github.com/grosser/rpx_now)
+		#
 		def rpx_popup(options = {})
-			params = (
-				{ :authenticity_token => form_authenticity_token, :add_rpx => options[:add_rpx] }.collect { |n| "#{n[0]}=#{ u(n[1]) }" if n[1] }
-			).compact.join('&')
-			unobtrusive = options[:unobtrusive].nil? ? true : options[:unobtrusive]
-			return_url = options[:return_url] + '?' + params 
-			return_url = u( return_url ) if unobtrusive # double-encoding required only if unobtrusive mode used
-			RPXNow.popup_code(
-  				options[:link_text], 
-  				options[:app_name],
-  				return_url,
-  				:unobtrusive=>unobtrusive
-  			) 
+			options = { :unobtrusive => true, :add_rpx => false }.merge( options )
+			unobtrusive = options[:unobtrusive]
+			add_rpx = options.delete( :add_rpx )
+			app_name = options.delete( :app_name )
+			link_text = options.delete( :link_text )
+			return_url = options.delete( :return_url ) + '?' + (
+				{ :authenticity_token => form_authenticity_token, :add_rpx => add_rpx }.collect { |n| "#{n[0]}=#{ u(n[1]) }" if n[1] }
+			).compact.join('&') 
+			
+			# as of rpx_now 0.6.11, there is still an issue with url-encoding of the return path containing additional parameters 
+			# (as we are using here to get the form_authenticity_token returned.
+			# To get around this, we need to ensure hrefs are url-encoded, 
+			# while the link provided to the rpx_now javascript popup should _not_ be url-encoded
+			if unobtrusive
+  			RPXNow.popup_code( link_text, app_name,	u( return_url ), options	)
+			else
+  			RPXNow.popup_code( link_text, app_name,	u( return_url ), options.merge( :unobtrusive => true )	) +
+        RPXNow.popup_source(app_name, return_url, options ) 
+      end
 		end
 
 	end
