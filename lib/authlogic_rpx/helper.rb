7393eebf84ad7818915ed1692feb5d68a0d704a2
tardate <gallagher.paul@gmail.com>
2010-01-17 10:36:09 +0800

    refactor helper. Ensure embed code passes options to rpx_now

--- lib/authlogic_rpx/helper.rb
+++ lib/authlogic_rpx/helper.rb
@@ -7,11 +7,12 @@ module AuthlogicRpx
 		#   * <tt>return_url:</tt> url for the RPX callback (e.g. user_sessions_url)
 		#   * <tt>add_rpx:</tt> if true, requests RPX callback to add to current session. Else runs normal authentication process (default)
 		#
+		# The options hash may include other options as supported by rpx_now (see http://github.com/grosser/rpx_now)
+		#
 		def rpx_embed(options = {})
-			params = (
-				{ :authenticity_token => form_authenticity_token, :add_rpx => options[:add_rpx] }.collect { |n| "#{n[0]}=#{ u(n[1]) }" if n[1] }
-			).compact.join('&')
-			RPXNow.embed_code(options[:app_name], options[:return_url] + '?' + params )
+		  app_name = options.delete( :app_name )
+			token_url = build_token_url!( options )
+			RPXNow.embed_code(app_name, token_url, options )
 		end
 
 		# helper to insert a link to pop-up RPX login
@@ -26,16 +27,18 @@ module AuthlogicRpx
 		#
 		def rpx_popup(options = {})
 			options = { :unobtrusive => true, :add_rpx => false }.merge( options )
-			unobtrusive = options[:unobtrusive]
-			add_rpx = options.delete( :add_rpx )
 			app_name = options.delete( :app_name )
 			link_text = options.delete( :link_text )
-			return_url = options.delete( :return_url ) + '?' + (
-				{ :authenticity_token => form_authenticity_token, :add_rpx => add_rpx }.collect { |n| "#{n[0]}=#{ u(n[1]) }" if n[1] }
-			).compact.join('&') 
-			
-			RPXNow.popup_code( link_text, app_name,	return_url, options	)
+			token_url = build_token_url!( options ) 
+			RPXNow.popup_code( link_text, app_name,	token_url, options	)
 		end
 
+    private
+    
+    def build_token_url!( options )
+      options.delete( :return_url ) + '?' + (
+        { :authenticity_token => form_authenticity_token, :add_rpx => options.delete( :add_rpx ) }.collect { |n| "#{n[0]}=#{ u(n[1]) }" if n[1] }
+			).compact.join('&')
+    end
 	end
 end
