842600656bc893e25ff9d24b98d00b4131473969
Paul Gallagher <gallagher.paul@gmail.com>
2010-09-28 02:31:01 +0800

    update gemspec for 1.2.0

--- lib/authlogic_rpx/version.rb
+++ lib/authlogic_rpx/version.rb
@@ -40,8 +40,8 @@ module AuthlogicRpx
     end
 
     MAJOR = 1
-    MINOR = 1
-    TINY  = 1
+    MINOR = 2
+    TINY  = 0
 
     # The current version as a Version instance
     CURRENT = new(MAJOR, MINOR, TINY)
